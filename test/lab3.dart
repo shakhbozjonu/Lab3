5c59f7b0b579da37f0d2fa629d4b92f619668049
shakhbozjonu <115206579+shakhbozjonu@users.noreply.github.com>
2023-10-31 00:44:50 +0500

    Update lab3.dart

--- test/lab3.dart
+++ test/lab3.dart
@@ -3,6 +3,7 @@ void main() {
   print('#1');
   print('Hi! I am Shakhbozjon Usmonov, I started coding in Dart!');
 
+  //2
   print('#2');
   int number = 10; // Change this to the number you want to check
   print(checkOddEven(number));
@@ -34,13 +35,49 @@ void main() {
   print("The original list is: $a");
   print("The new list with squared numbers is: $b");
 
+  //5
+  print('#5');
+  int num = 4; // change to check
+  printDivisors(number);
 
+  //6
+  print('#6');
+  int num1 = 7; // change to check
+  print(isPrime(num1) ? "The number $num1 is prime" : "The number $num1 is not prime");
 }
-//2
+
+
+
 String checkOddEven(int number) {
   if (number % 2 == 0) {
     return "The number $number is even";
   } else {
     return "The number $number is odd";
   }
+}
+
+void printDivisors(int num) {
+  List<int> divisors = [];
+
+  for (int i = 2; i <= num; i++) {
+    if (num % i == 0) {
+      divisors.add(i);
+    }
+  }
+
+  print(" divisors of $num : $divisors");
+}
+
+bool isPrime(int num1) {
+  if (num1 < 2) {
+    return false;
+  }
+
+  for (int i = 2; i * i <= num1; i++) {
+    if (num1 % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
